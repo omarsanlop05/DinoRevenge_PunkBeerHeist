59b5c8545fa8a4e7b277b7da77606b5b8e5acbd8
Omar A <0262155@up.edu.mx>
2025-10-22 17:35:24 -0600

    cameras adjustments

--- Assets/Scripts/Camera/ParallaxBackground.cs
+++ Assets/Scripts/Camera/ParallaxBackground.cs
@@ -5,12 +5,14 @@ public class ParallaxBackground : MonoBehaviour
 {
 
     private float StartPosition, length;
-    public GameObject cam;
+    //public GameObject cam;
+    private Transform camTransform;
     public float parallaxEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        camTransform = Camera.main.transform;
         StartPosition = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
@@ -18,8 +20,8 @@ public class ParallaxBackground : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distance = cam.transform.position.x * parallaxEffect;
-        float movement = cam.transform.position.x * (1 - parallaxEffect);
+        float distance = camTransform.transform.position.x * parallaxEffect;
+        float movement = camTransform.transform.position.x * (1 - parallaxEffect);
 
         transform.position = new Vector3(StartPosition + distance, transform.position.y, transform.position.z);
 
